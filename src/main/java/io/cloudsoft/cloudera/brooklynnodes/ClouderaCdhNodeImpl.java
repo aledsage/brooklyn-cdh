8e349b6a0e161d13d8b0ee74f1537e55070e0d79
Aled Sage <aled.sage@gmail.com>
2013-05-08 13:57:48 +0100

    Convert ClouderaCdhNodeImpl to java

--- src/main/java/io/cloudsoft/cloudera/brooklynnodes/ClouderaCdhNodeImpl.java
+++ src/main/java/io/cloudsoft/cloudera/brooklynnodes/ClouderaCdhNodeImpl.java
@@ -1,49 +1,78 @@
 package io.cloudsoft.cloudera.brooklynnodes;
 
-import static brooklyn.util.GroovyJavaMethods.elvis
-import groovy.transform.InheritConstructors
+import static brooklyn.util.JavaGroovyEquivalents.groovyTruth;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
-import org.jclouds.compute.domain.OsFamily
-import org.jclouds.compute.domain.TemplateBuilder;
+import org.jclouds.compute.domain.OsFamily;
 import org.jclouds.googlecomputeengine.GoogleComputeEngineApiMetadata;
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-
-import brooklyn.entity.basic.BasicConfigurableEntityFactory
-import brooklyn.entity.basic.ConfigurableEntityFactory
-import brooklyn.entity.basic.Description
-import brooklyn.entity.basic.NamedParameter
-import brooklyn.entity.basic.SoftwareProcessImpl
-import brooklyn.entity.basic.lifecycle.ScriptHelper
-import brooklyn.event.feed.function.FunctionFeed
-import brooklyn.event.feed.function.FunctionPollConfig
-import brooklyn.location.MachineProvisioningLocation
+import brooklyn.entity.Entity;
+import brooklyn.entity.basic.BasicConfigurableEntityFactory;
+import brooklyn.entity.basic.ConfigurableEntityFactory;
+import brooklyn.entity.basic.Description;
+import brooklyn.entity.basic.NamedParameter;
+import brooklyn.entity.basic.SoftwareProcessImpl;
+import brooklyn.entity.basic.lifecycle.ScriptHelper;
+import brooklyn.entity.basic.lifecycle.ScriptRunner;
+import brooklyn.event.feed.function.FunctionFeed;
+import brooklyn.event.feed.function.FunctionPollConfig;
+import brooklyn.location.MachineProvisioningLocation;
 import brooklyn.location.jclouds.JcloudsLocation;
 import brooklyn.location.jclouds.JcloudsLocationConfig;
-import brooklyn.location.jclouds.templates.PortableTemplateBuilder
+import brooklyn.location.jclouds.templates.PortableTemplateBuilder;
+import brooklyn.util.MutableMap;
+import brooklyn.util.MutableSet;
 
 import com.google.common.base.Charsets;
-import com.google.common.base.Functions
+import com.google.common.base.Functions;
+import com.google.common.base.Predicate;
 import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.io.Files;
 
-@InheritConstructors
 public class ClouderaCdhNodeImpl extends SoftwareProcessImpl implements ClouderaCdhNode {
 
-    private static final Logger log = LoggerFactory.getLogger(ClouderaCdhNodeImpl.class)
+    private static final Logger log = LoggerFactory.getLogger(ClouderaCdhNodeImpl.class);
     
     public static ConfigurableEntityFactory<ClouderaCdhNodeImpl> newFactory() { 
         return new BasicConfigurableEntityFactory<ClouderaCdhNodeImpl>(ClouderaCdhNodeImpl.class);
     }
     
-    public Class getDriverInterface() { return ClouderaCdhNodeSshDriver.class; }
+    public ClouderaCdhNodeImpl() {
+    }
+
+    public ClouderaCdhNodeImpl(Map flags) {
+    	super(flags);
+    }
+
+    public ClouderaCdhNodeImpl(Entity parent) {
+    	super(parent);
+    }
+
+    public ClouderaCdhNodeImpl(Map flags, Entity parent) {
+    	super(flags, parent);
+    }
+    
+
+    @Override
+    public Class getDriverInterface() {
+    	return ClouderaCdhNodeSshDriver.class;
+	}
+    
+    @Override
+    public ClouderaCdhNodeDriver getDriver() {
+    	return (ClouderaCdhNodeDriver) super.getDriver();
+    }
     
     protected Map<String,Object> obtainProvisioningFlags(MachineProvisioningLocation location) {
         /*
@@ -95,9 +124,10 @@ public class ClouderaCdhNodeImpl extends SoftwareProcessImpl implements Cloudera
 
     // 7180, 7182, 8088, 8888, 50030, 50060, 50070, 50090, 60010, 60020, 60030
     protected Collection<Integer> getRequiredOpenPorts() {
-        Set result = [22, 2181, 7180, 7182, 8088, 8888, 50030, 50060, 50070, 50090, 60010, 60020, 60030];
-        result.addAll(super.getRequiredOpenPorts());
-        return result;
+        return MutableSet.<Integer>builder()
+        		.addAll(22, 2181, 7180, 7182, 8088, 8888, 50030, 50060, 50070, 50090, 60010, 60020, 60030)
+        		.addAll(super.getRequiredOpenPorts())
+				.build();
     }
     
     public void connectSensors() {
@@ -112,46 +142,50 @@ public class ClouderaCdhNodeImpl extends SoftwareProcessImpl implements Cloudera
         FunctionFeed feed = FunctionFeed.builder()
                 .entity(this)
                 .poll(new FunctionPollConfig<Boolean,Boolean>(SERVICE_UP)
-                .period(30, TimeUnit.SECONDS)
-                .callable(new Callable<Boolean>() {
-                    @Override
-                    public Boolean call() throws Exception {
-                        try { return getManagedHostId()!=null }
-                        catch (Exception e) { return false; }
-                    }
-                })
-                .onError(Functions.constant(false))
-                )
-                .poll(new FunctionPollConfig<List,List>(CDH_HOST_ID)
-                .period(30, TimeUnit.SECONDS)
-                .callable(new Callable<String>() {
-                    @Override
-                    public String call() throws Exception {
-                        return getManagedHostId();
-                    }
-                })
-                .onError(Functions.constant(false))
-                )
+		                .period(30, TimeUnit.SECONDS)
+		                .callable(new Callable<Boolean>() {
+		                    @Override
+		                    public Boolean call() throws Exception {
+		                    	return getManagedHostId()!=null;
+		                    }
+		                })
+		                .onError(Functions.constant(false))
+		                )
+                .poll(new FunctionPollConfig<String,String>(CDH_HOST_ID)
+		                .period(30, TimeUnit.SECONDS)
+		                .callable(new Callable<String>() {
+		                    @Override
+		                    public String call() throws Exception {
+		                        return getManagedHostId();
+		                    }
+		                })
+		                .onError(Functions.constant((String)null))
+		                )
                 .build();
     }
     
     public String getManagedHostId() {
-        def managedHosts = getConfig(MANAGER)?.getAttribute(ClouderaManagerNode.MANAGED_HOSTS);
-        if (!managedHosts) return null;
+        ClouderaManagerNode manager = getConfig(MANAGER);
+        List<String> managedHosts = (manager == null) ? null : manager.getAttribute(ClouderaManagerNode.MANAGED_HOSTS);
+        if (managedHosts == null || managedHosts.isEmpty()) return null;
         String hostname = getAttribute(HOSTNAME);
-        if (hostname && managedHosts.contains(hostname)) return hostname;
-        String privateHostname = getAttribute(PRIVATE_HOSTNAME);
-        if (privateHostname) {
+        if (groovyTruth(hostname) && managedHosts.contains(hostname)) return hostname;
+        final String privateHostname = getAttribute(PRIVATE_HOSTNAME);
+        if (groovyTruth(privateHostname)) {
             // manager might view it as ip-10-1-1-1.ec2.internal whereas node knows itself as just ip-10-1-1-1
             // TODO better might be to compare private IP addresses of this node with IP of managed nodes at CM  
-            def pm = managedHosts.find { it.startsWith(privateHostname) }
-            if (pm) return pm;
+            String pm = Iterables.find(managedHosts, new Predicate<String>() {
+            	public boolean apply(String input) {
+            		return input.startsWith(privateHostname);
+            	}},
+            	null);
+            if (groovyTruth(pm)) return pm;
         }
         return null;
     }
 
     public ScriptHelper newScript(String summary) {
-        return new ScriptHelper(driver, summary);
+        return new ScriptHelper((ScriptRunner)getDriver(), summary);
     }
 
     /**
@@ -163,25 +197,25 @@ public class ClouderaCdhNodeImpl extends SoftwareProcessImpl implements Cloudera
         new File(targetDir).mkdir();
         // TODO allow wildcards, or batch on server then copy down?
         int i=0;
-        for (role in ["datanode","namenode","master","regionserver"]) {
+        for (String role : ImmutableList.of("datanode","namenode","master","regionserver")) {
             try {
-                ((ClouderaCdhNodeDriver)driver).machine.copyFrom(sshTries:1,
-                    "/tmp/${role}-metrics.out", targetDir+"/${role}-metrics.out");
+                ((ClouderaCdhNodeSshDriver)getDriver()).getMachine().copyFrom(MutableMap.of("sshTries", 1),
+                    "/tmp/"+role+"-metrics.out", targetDir+"/"+role+"-metrics.out");
             } catch (Exception e) {
                 //not serious, file probably doesn't exist
-                log.debug("Unable to copy /tmp/${role}-metrics.out from ${this} (file may not exist): "+e);
+                log.debug("Unable to copy /tmp/"+role+"-metrics.out from "+this+" (file may not exist): "+e);
             }
         }
-        for (role in ["mr","jvm"]) {
+        for (String role : ImmutableList.of("mr","jvm")) {
             try {
-                ((ClouderaCdhNodeDriver)driver).machine.copyFrom(sshTries:1,
-                    "/tmp/${role}metrics.log", targetDir+"/${role}metrics.log");
+                ((ClouderaCdhNodeSshDriver)getDriver()).getMachine().copyFrom(MutableMap.of("sshTries", 1),
+                    "/tmp/"+role+"metrics.log", targetDir+"/"+role+"metrics.log");
             } catch (Exception e) {
                 //not serious, file probably doesn't exist
-                log.debug("Unable to copy /tmp/${role}metrics.log from ${this} (file may not exist): "+e);
+                log.debug("Unable to copy /tmp/"+role+"metrics.log from "+this+" (file may not exist): "+e);
             }
         }
-        log.debug("Copied ${i} metrics files from ${this}");
+        log.debug("Copied {} metrics files from {}", i, this);
         return targetDir;
     }
     
